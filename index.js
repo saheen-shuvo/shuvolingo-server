4f2da2956dc2cfdc8d9f2bd30fb24fdd691465db
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-01-26 01:00:01 +0600

    Tutors Data fetched

--- index.js
+++ index.js
@@ -26,6 +26,23 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // TUTORS RELATED API's
+const tutorsCollection = client.db('Shuvolingo').collection('tutors');
+
+app.get('/tutors', async(req, res) => {
+    const cursor = tutorsCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+})
+
+
+
+
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
