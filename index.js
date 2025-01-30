f48a3ce4a984d9c6e93c12a645be1dc9a68cf2bb
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-01-31 02:30:15 +0600

    My booked tutors added

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42yqa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,6 +32,7 @@ async function run() {
 
     // TUTORS RELATED API's
 const tutorsCollection = client.db('Shuvolingo').collection('tutors');
+const bookedTutorsCollection = client.db('Shuvolingo').collection('bookedTutors');
 
 app.get('/tutors', async(req, res) => {
   const email = req.query.email;
@@ -57,6 +61,42 @@ app.post('/tutors', async(req, res) => {
   const result = await tutorsCollection.insertOne(newTutor);
   res.send(result);
 })
+
+app.post('/bookedtutors', async (req, res) => {
+  try {
+    const { tutorId, image, language, price, tutorEmail, email } = req.body;
+    const bookedTutor = {
+      tutorId: new ObjectId(tutorId),
+      image,
+      language,
+      price,
+      tutorEmail,
+      email,
+    };
+    const result = await bookedTutorsCollection.insertOne(bookedTutor);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ message: 'Error booking tutor', error });
+  }
+});
+
+
+app.get('/bookedtutors', async(req, res) => {
+  const email = req.query.email;
+  const query = {email: email};
+  const result = await bookedTutorsCollection.find(query).toArray();
+  res.send(result);
+})
+
+app.put('/tutors/review/:id', async(req, res) => {
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)};
+  const update = {$inc: {review: 1}};
+  const options = {returnOriginal: false};
+  const result = await tutorsCollection.findOneAndUpdate(filter, update, options);
+  res.send(result)
+})
+
 
 app.put("/tutors/:id", async (req, res) => {
   const id = req.params.id;
